b8616d37e8de375474f7c8aef592d4195730939c
aashay <aashayborgaonkar@gmail.com>
2017-11-28 12:08:08 -0600

    Included main method in the Stack class

--- Stacks/Stack.java
+++ Stacks/Stack.java
@@ -41,4 +41,15 @@ public class Stack {
 	public boolean isEmpty() {
 		return (top == -1);
 	}
+	
+	public static void main(String args[]) {
+		Stack stack = new Stack(10);
+		stack.push(2);
+		stack.push(12);
+		stack.push(3);
+		stack.push(6);
+		stack.push(34);
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+	}
 }
